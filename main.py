a0de7435635d9794d8d1e9875c924891735816f0
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-16 22:14:31 -0400

    Primer commit

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 from funciones import(registrar_taller,
-                      consultar_taller,
+                      listar_taller,
+                      buscar_taller,
                       actuaizar_taller,
                       eliminar_taller
                       )
 
 talleres = []
-while True:
+opcion = 0
+
+
+while opcion != 6:
+
     print("=== SISTEMA DE TALLERES ACADÉMICOS DUOC ===")
     print("1. Registrar taller")
     print("2. Listar talleres")
     print("3. Buscar taller")
-    print("3. Buscar taller")
     print("4. Actualizar taller")
     print("5. Eliminar taller")
     print("6. Salir")
+
+    try:
+        opcion = int(input("Ingrese una opcion: "))
+    except:
+        print("La opcion debe de ser numerica")
+        continue
+
+    if opcion == 1:
+        registrar_taller(talleres)
+    elif opcion == 2:
+       listar_taller(talleres)
+    elif opcion == 3:
+        buscar_taller(talleres)
+    elif opcion == 4:
+        actuaizar_taller(talleres)
+    elif opcion == 5:
+        eliminar_taller(talleres)
+    elif opcion == 6:
+        print("Gracias por usar el sistema")
