5e61b7cb8e48eb1d50729f50f2c8396a94021cdb
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-16 21:57:55 -0400

    Primer commit

---
+++ funciones.py
@@ -0,0 +1,17 @@
+def registrar_taller(talleres):
+    print("---registro de taller---")
+    codigo = input("Ingrese el codigo del taller: ")
+    nombre = input("Ingrese el nombre del taller: ")
+    docente = input("Ingrese el nombre del docente responsable: ")
+    cupo_m = int(input("Ingrese el cupo maximo de estudiantes: "))
+
+def listar_taller(talleres):
+
+
+def buscar_taller(talleres):
+
+
+def actuaizar_taller(talleres):
+
+
+def eliminar_taller(talleres):
---
+++ main.py
@@ -0,0 +1,16 @@
+from funciones import(registrar_taller,
+                      consultar_taller,
+                      actuaizar_taller,
+                      eliminar_taller
+                      )
+
+talleres = []
+while True:
+    print("=== SISTEMA DE TALLERES ACADÉMICOS DUOC ===")
+    print("1. Registrar taller")
+    print("2. Listar talleres")
+    print("3. Buscar taller")
+    print("3. Buscar taller")
+    print("4. Actualizar taller")
+    print("5. Eliminar taller")
+    print("6. Salir")
